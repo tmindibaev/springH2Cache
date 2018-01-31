6905cb0916ce77cffbfba2142631ae1fcd32ba0e
tmindibaev <timeoffat@gmail.com>
2018-01-31 18:03:05 +0300

    Пример обработки <Object>, формат xml, переделать в <JSONObject>

---
+++ src/main/java/com/h2test/sprngbt/GlobalCustomExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.h2test.sprngbt;
+
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalDefaultExceptionHandler {
+    public static final String DEFAULT_ERROR_VIEW = "error";
+
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView
+    defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        /*if (AnnotationUtils.findAnnotation
+                (e.getClass(), ResponseStatus.class) != null)
+            throw e;*/
+
+        // Otherwise setup and send the user to a default error-view.
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName(DEFAULT_ERROR_VIEW);
+        return mav;
+    }
+}
---
+++ src/main/java/com/h2test/sprngbt/service/ErrorCode.java
@@ -0,0 +1,4 @@
+package com.h2test.sprngbt.service;
+
+public enum ErrorCode {
+}
